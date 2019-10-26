6743a89616f5f98f23642d828d3512b04d60d9f8
Rahul Makhija <rahuls360@gmail.com>
2019-10-26 18:42:37 +0530

    add Tabs for countries

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,59 @@
 import React from 'react';
 import Header from './Header';
 
+const tabData = [
+  {
+    value: 0,
+    id: "asia",
+    name: "Asia"
+  },
+  {
+    value: 1,
+    id: "europe",
+    name: "Europe"
+  },
+  {
+    value: 2,
+    id: "america",
+    name: "America"
+  },
+]
+
 class Home extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      tab: 0
+    }
+  }
+
+  handleTab = (value, id) => {
+    this.setState({tab: value})
+  }
+
   render(){
     return (
       <div>
         <Header />
-        <div>
-          hello
-        </div>
+        <ul style={styles.ul}>
+          {tabData.map(tab => (
+            <li onClick={() => this.handleTab(tab.value, tab.id)} style={styles.li}>{tab.name}</li>
+          ))}
+        </ul>
       </div>
     );
+  }
+}
+
+const styles = {
+  ul: {
+    display: "flex",
+    listStyle: 'none',
+  },
+  li: {
+    marginRight: 10,
+    cursor: 'pointer'
   }
 }
 
